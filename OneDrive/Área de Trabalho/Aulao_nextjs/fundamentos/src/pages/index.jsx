37ecbeb136168aefc5a92f0d01f8d7f50d52ab1a
Marcos <marcosjrsouza@hotmail.com>
2022-07-02 13:32:22 -0300

    novo Repositorio

---
+++ OneDrive/Área de Trabalho/Aulao_nextjs/fundamentos/src/pages/index.jsx
@@ -0,0 +1,9 @@
+export default function Inicio() {
+    return (
+        <div>
+            <h1>Fundamentos de next.js & React</h1>
+            <h2>Estudando este frameWork</h2>
+
+        </div>
+    )
+}
